e65f0e4ecf2e0cc755271c652ed046b2e679e704
Michael Shuff <mjshuff23@gmail.com>
2020-10-21 14:06:56 -0400

    finished Phase 2

--- src/actions/gifActions.js
+++ src/actions/gifActions.js
@@ -1,7 +1,12 @@
 // TODO: Import all of your importing your API util function
-
 // TODO: Set and export a constant for your `RECEIVE_GIFS` action type
+export const RECEIVE_GIFS = 'RECEIVE_GIFS';
 
 // TODO: Write a function that returns your `action` object literal
-
+const receiveGifts = gifs => (
+  {
+    type: RECEIVE_GIFS,
+    gifs,
+  }
+);
 // TODO: Write a thunk action creator
