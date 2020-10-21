05c656953a7ae8b664ee20114caa5b27cf22406b
Michael Shuff <mjshuff23@gmail.com>
2020-10-21 15:15:51 -0400

    finished phase 5

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import App from './App';
+import { Provider } from 'react-redux';
 
-const Root = () => (
-  <App />
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 export default Root;
--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 // TODO: Import `createStore` from `redux`
+import { createStore, applyMiddleware } from 'redux';
 // TODO: Import middleware
 // TODO: Import `rootReducer`
+import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
 
 // TODO: Define a `configureStore` function
+const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
 
 // TODO: Export the `configureStore` function
+export default configureStore;
