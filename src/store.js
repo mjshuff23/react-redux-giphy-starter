0ec17740835d273e399a56ad83ef41c5efb3137f
Michael Shuff <mjshuff23@gmail.com>
2020-10-21 15:17:41 -0400

    finished phase 6

--- src/store.js
+++ src/store.js
@@ -4,9 +4,10 @@ import { createStore, applyMiddleware } from 'redux';
 // TODO: Import `rootReducer`
 import rootReducer from './reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { logger } from 'redux-logger';
 
 // TODO: Define a `configureStore` function
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk));
+const configureStore = () => createStore(rootReducer, applyMiddleware(thunk, logger));
 
 // TODO: Export the `configureStore` function
 export default configureStore;
