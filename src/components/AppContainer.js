de9098f046ce33785418b5380b7938270cc9a96e
Michael Shuff <mjshuff23@gmail.com>
2020-10-21 15:28:34 -0400

    finished phase 7

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -5,12 +5,14 @@ import App from './App';
 const mapStateToProps = state => {
   return {
     // TODO: Pass `gifs` prop
+    gifs: state.gifs,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
     // TODO: Pass `fetchGifs` prop
+    fetchGifs: (searchQuery) => fetchGifs(searchQuery),
   };
 };
 
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import { Provider } from 'react-redux';
+import AppContainer from './AppContainer';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <AppContainer />
   </Provider>
 );
 
